4bff80b3cb2aa7a311d9ad2ab7da89e757e44b35
Don Olmstead <don.j.olmstead@gmail.com>
2025-06-17 11:29:34 -0700

    client: support files/:key/meta endpoint
    Add ability to query the metadata of a file. Add all the associated models for the response.

---
+++ lib/src/models/role.dart
@@ -0,0 +1,11 @@
+import 'package:json_annotation/json_annotation.dart';
+
+/// Access policy for users who have a link to a file.
+enum Role {
+  @JsonValue('owner')
+  owner,
+  @JsonValue('editor')
+  editor,
+  @JsonValue('viewer')
+  viewer
+}
